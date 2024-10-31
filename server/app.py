546c7e49c294d8b8a1ab94cc9e057149ffa593e4
Ronney Arias <ronneyarias@gmail.com>
2024-10-31 01:47:30 -0500

    complete lab

--- server/app.py
+++ server/app.py
@@ -45,8 +45,43 @@ class PlantByID(Resource):
 
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
-        return make_response(jsonify(plant), 200)
 
+        return make_response(jsonify(plant), 200)
+    
+    def patch(self, id):
+        record = Plant.query.filter(Plant.id == id).first()
+        for attr in request.form:
+            setattr(record, attr, request.form[attr])
+
+        record.is_in_stock = False
+
+        db.session.add(record)
+        db.session.commit()
+
+        response_dict = record.to_dict()
+        
+        response = make_response(
+            jsonify(response_dict),
+            200
+        )
+
+        return response
+
+    
+    def delete(self, id):
+        record = Plant.query.filter(Plant.id == id).first()
+
+        db.session.delete(record)
+        db.session.commit()
+
+        response_dict = {"message": "no content"}
+
+        response = make_response(
+            response_dict,
+            204
+        )
+
+        return response
 
 api.add_resource(PlantByID, '/plants/<int:id>')
 
